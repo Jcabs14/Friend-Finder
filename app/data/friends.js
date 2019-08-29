1a72f93b3b418ed6181addb0e2b8ae6d3f56a4e9
Jared Cabanilla <Jcabanilla@gmail.com>
2019-08-29 14:55:26 -0700

    added samples in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,45 @@
 // ===============================================================================
 
 var friendsArray = [
-  ];
+
+  {
+    "name": "Captain America",
+    "photo": "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/47/1542988952-captain-america-civil-war-128.png",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+
+  {
+    "name": "Iron Man",
+    "photo": "https://7lwy5tgst9-flywheel.netdna-ssl.com/wp-content/uploads/2018/01/Iron-Man.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+
+  },
   
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsArray;
   
+
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
